0390527b65cdeba5a69f02834fd4717e8c494be2
祈媗 <C112181138@nkust.edu.tw>
2026-03-12 11:39:21 +0800

    Add Q3_Turtle class with details display method

---
+++ week03/Q3_Turtle.java
@@ -0,0 +1,25 @@
+public class Q3_Turtle {
+
+    
+    String species;   
+    int age;          
+
+    Q3_Turtle(String species, int age) {
+        this.species = species;
+        this.age = age;
+    }
+
+
+    void showDetails() {
+        System.out.println("品種：" + species + "，年紀：" + age + "歲");
+    }
+
+
+    public static void main(String[] args) {
+
+        Q3_Turtle myTurtle = new Q3_Turtle("綠蠵龜", 50);
+
+    
+        myTurtle.showDetails();
+    }
+}
