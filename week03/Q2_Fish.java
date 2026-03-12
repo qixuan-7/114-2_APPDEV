2a226407072baa44280806e7c2bdf7695b7c78cc
祈媗 <C112181138@nkust.edu.tw>
2026-03-12 11:53:03 +0800

    Add files via upload

--- week03/Q2_Fish.java
+++ week03/Q2_Fish.java
@@ -1,16 +1,21 @@
 public class Q2_Fish {
 
-    String name;     
+   
+    String name;      
     double weight;    
 
+   
     void displayInfo() {
         System.out.println("這隻魚的名字是：" + name + "，重量是：" + weight + "公斤");
     }
 
+  
     public static void main(String[] args) {
 
+       
         Q2_Fish myFish = new Q2_Fish();
-      
+
+       
         myFish.name = "黑鮪魚";
         myFish.weight = 250.5;
 
